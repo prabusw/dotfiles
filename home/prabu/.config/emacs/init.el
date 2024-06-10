e45be6d17d8fbb9b5bcf540d026f558ee01ab8e3
Prabu Anand Kalivaradhan <kprabuanand@gmail.com>
2024-06-10 12:32:22 +0530

    projectile added to init.el

--- home/prabu/.config/emacs/init.el
+++ home/prabu/.config/emacs/init.el
@@ -712,6 +712,15 @@ e.g. Sunday, September 17, 2000."
         ;; ...
         ))
 )
+;;;; projectile
+(use-package projectile
+  :ensure t
+  :init
+  (projectile-mode +1)
+  :bind (:map projectile-mode-map
+              ("s-p" . projectile-command-map)
+              ("C-c p" . projectile-command-map)))
+
 
 
 
