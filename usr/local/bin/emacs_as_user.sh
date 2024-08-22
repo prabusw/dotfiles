6bae61af9616a0ba2ca9a4069cc5fd3504386baa
Prabu Anand Kalivaradhan <kprabuanand@gmail.com>
2024-08-22 12:19:36 +0530

    removed apkdiff and related files

--- usr/local/bin/emacs_as_user.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Use the current user from the environment variable
-target_user="${USER:-$(whoami)}"
-# Set environment variables to ensure Emacs uses the correct configuration
-export HOME="/home/$target_user"
-export USER="$target_user"
-# Run Emacs as the specified user
-exec doas -u "$target_user" emacs "$@"
