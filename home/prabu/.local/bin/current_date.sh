3ab1ffed93e61b3e7ce398b99d73a36589e353a1
Prabu Anand Kalivaradhan <kprabuanand@gmail.com>
2024-09-11 20:54:47 +0530

    fixed the bash env flag in current_date.sh

--- home/prabu/.local/bin/current_date.sh
+++ home/prabu/.local/bin/current_date.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 # notify-send "    $(date +'%a %d')"
-date +'%A %F %H:%M:%S' | wl-copy
 sway-notify "    $(date +'%A %F')"
+date +'%A %F %H:%M:%S' | wl-copy
 # sway-notify -i calendar "$(date "+%A %F")"
